40ebc422b3acba35d9fe9926e1780d7dc429c3a7
bhanujaya <bhanujayasmiz@gmail.com>
2024-09-18 13:49:17 +0700

    login-register-project

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Register() {
     return (
       <>
-        <h1 className="text-3xl">Login Page</h1>
+        <h1 className="text-3xl">Register Page</h1>
         <h2>
           <Link href="/login">Don't have an Account?</Link>
         </h2>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'customGray': '#fafbfc',
+        'customDarkBlue': '#111F34',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      boxShadow: {
+        '3xl': '0 20px 25px rgba(0, 0, 0, 0.25)',
+        '4xl': '0 10px 70px rgba(0, 0, 0, 0.25)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'login-background': "url('/Background.png')",
       },
     },
   },
